67c0bbc325f57a5b4295a4aefcc6a0780c7b73cb
Alexey Umnov <alexeyum@gmail.com>
2017-11-09 22:16:43 +0300

    Homework 1 fixes and refactoring

---
+++ 2017-fall/homeworks-practice/homework-practice-01-templates/functions.py
@@ -0,0 +1,84 @@
+def prod_non_zero_diag(x):
+    """Compute product of nonzero elements from matrix diagonal.
+
+    input:
+    x -- 2-d numpy array
+    output:
+    product -- integer number
+
+
+    Not vectorized implementation.
+    """
+
+    pass
+
+
+def are_multisets_equal(x, y):
+    """Return True if both vectors create equal multisets.
+
+    input:
+    x, y -- 1-d numpy arrays
+    output:
+    True if multisets are equal, False otherwise -- boolean
+
+    Not vectorized implementation.
+    """
+
+    pass
+
+
+def max_after_zero(x):
+    """Find max element after zero in array.
+
+    input:
+    x -- 1-d numpy array
+    output:
+    maximum element after zero -- integer number
+
+    Not vectorized implementation.
+    """
+
+    pass
+
+
+def convert_image(img, coefs):
+    """Sum up image channels with weights from coefs array
+
+    input:
+    img -- 3-d numpy array (H x W x 3)
+    coefs -- 1-d numpy array (length 3)
+    output:
+    img -- 2-d numpy array
+
+    Not vectorized implementation.
+    """
+
+    pass
+
+
+def run_length_encoding(x):
+    """Make run-length encoding.
+
+    input:
+    x -- 1-d numpy array
+    output:
+    elements, counters -- integer iterables
+
+    Not vectorized implementation.
+    """
+
+    pass
+
+
+def pairwise_distance(x, y):
+    """Return pairwise object distance.
+
+    input:
+    x, y -- 2d numpy arrays
+    output:
+    distance array -- 2d numpy array
+
+    Not vectorized implementation.
+    """
+
+    pass
