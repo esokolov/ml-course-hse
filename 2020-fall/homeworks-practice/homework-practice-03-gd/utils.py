b316cca5a7bb11cb5e4eed95e997d92d594639ec
birshert <birshertalexey21@gmail.com>
2020-10-05 11:58:28 +0300

    HW 3 gradient descent

---
+++ 2020-fall/homeworks-practice/homework-practice-03-gd/utils.py
@@ -0,0 +1,390 @@
+from __future__ import annotations
+
+import numpy as np
+
+
+s0_default: float = 1
+p_default: float = 0.5
+
+batch_size_default: int = 1
+
+alpha_default: float = 0.1
+eps_default: float = 1e-8
+
+mu_default = 1e-2
+
+tolerance_default: float = 1e-3
+max_iter_default: int = 1000
+
+
+class BaseDescent:
+    """
+    A base class and examples for all functions
+    """
+
+    def __init__(self):
+        self.w = None
+
+    def step(self, X: np.ndarray, y: np.ndarray, iteration: int) -> np.ndarray:
+        """
+        Descent step
+        :param iteration: iteration number
+        :param X: objects' features
+        :param y: objects' targets
+        :return: difference between weights
+        """
+        return self.update_weights(self.calc_gradient(X, y), iteration)
+
+    def update_weights(self, gradient: np.ndarray, iteration: int) -> np.ndarray:
+        """
+        Example for update_weights function
+        :param iteration: iteration number
+        :param gradient: gradient
+        :return: weight difference: np.ndarray
+        """
+        pass
+
+    def calc_gradient(self, X: np.ndarray, y: np.ndarray) -> np.ndarray:
+        """
+        Example for calc_gradient function
+        :param X: objects' features
+        :param y: objects' targets
+        :return: gradient: np.ndarray
+        """
+        pass
+
+
+class GradientDescent(BaseDescent):
+    """
+    Full gradient descent class
+    """
+
+    def __init__(self, w0: np.ndarray, lambda_: float, s0: float = s0_default, p: float = p_default):
+        """
+        :param w0: weight initialization
+        :param lambda_: learning rate parameter (float)
+        :param s0: learning rate parameter (float)
+        :param p: learning rate parameter (float)
+        """
+        super().__init__()
+        self.eta = lambda k: lambda_ * (s0 / (s0 + k)) ** p
+        self.w = np.copy(w0)
+
+    def update_weights(self, gradient: np.ndarray, iteration: int) -> np.ndarray:
+        """
+        Changing weights with respect to gradient
+        :param iteration: iteration number
+        :param gradient: gradient
+        :return: weight difference: np.ndarray
+        """
+        # TODO: implement updating weights function
+        raise NotImplementedError('GradientDescent update_weights function not implemented')
+
+    def calc_gradient(self, X: np.ndarray, y: np.ndarray) -> np.ndarray:
+        """
+        Getting objects, calculating gradient at point w
+        :param X: objects' features
+        :param y: objects' targets
+        :return: gradient: np.ndarray
+        """
+        # TODO: implement calculating gradient function
+        raise NotImplementedError('GradientDescent calc_gradient function not implemented')
+
+
+class StochasticDescent(BaseDescent):
+    """
+    Stochastic gradient descent class
+    """
+
+    def __init__(self, w0: np.ndarray, lambda_: float, s0: float = s0_default, p: float = p_default,
+                 batch_size: int = batch_size_default):
+        """
+        :param w0: weight initialization
+        :param lambda_: learning rate parameter (float)
+        :param s0: learning rate parameter (float)
+        :param p: learning rate parameter (float)
+        :param batch_size: batch size (int)
+        """
+        super().__init__()
+        self.eta = lambda k: lambda_ * (s0 / (s0 + k)) ** p
+        self.batch_size = batch_size
+        self.w = np.copy(w0)
+
+    def update_weights(self, gradient: np.ndarray, iteration: int) -> np.ndarray:
+        """
+        Changing weights with respect to gradient
+        :param iteration: iteration number
+        :param gradient: gradient estimate
+        :return: weight difference: np.ndarray
+        """
+        # TODO: implement updating weights function
+        raise NotImplementedError('StochasticDescent update_weights function not implemented')
+
+    def calc_gradient(self, X: np.ndarray, y: np.ndarray) -> np.ndarray:
+        """
+        Getting objects, calculating gradient at point w
+        :param X: objects' features
+        :param y: objects' targets
+        :return: gradient: np.ndarray
+        """
+        # TODO: implement calculating gradient function
+        raise NotImplementedError('StochasticDescent calc_gradient function not implemented')
+
+
+class MomentumDescent(BaseDescent):
+    """
+    Momentum gradient descent class
+    """
+
+    def __init__(self, w0: np.ndarray, lambda_: float, alpha: float = alpha_default, s0: float = s0_default,
+                 p: float = p_default):
+        """
+        :param w0: weight initialization
+        :param lambda_: learning rate parameter (float)
+        :param alpha: momentum coefficient
+        :param s0: learning rate parameter (float)
+        :param p: learning rate parameter (float)
+        """
+        super().__init__()
+        self.eta = lambda k: lambda_ * (s0 / (s0 + k)) ** p
+        self.alpha = alpha
+        self.w = np.copy(w0)
+        self.h = 0
+
+    def update_weights(self, gradient: np.ndarray, iteration: int) -> np.ndarray:
+        """
+        Changing weights with respect to gradient
+        :param iteration: iteration number
+        :param gradient: gradient estimate
+        :return: weight difference: np.ndarray
+        """
+        # TODO: implement updating weights function
+        raise NotImplementedError('MomentumDescent update_weights function not implemented')
+
+    def calc_gradient(self, X: np.ndarray, y: np.ndarray) -> np.ndarray:
+        """
+        Getting objects, calculating gradient at point w
+        :param X: objects' features
+        :param y: objects' targets
+        :return: gradient: np.ndarray
+        """
+        # TODO: implement calculating gradient function
+        raise NotImplementedError('MomentumDescent calc_gradient function not implemented')
+
+
+class Adagrad(BaseDescent):
+    """
+    Adaptive gradient algorithm class
+    """
+
+    def __init__(self, w0: np.ndarray, lambda_: float, eps: float = eps_default, s0: float = s0_default,
+                 p: float = p_default):
+        """
+        :param w0: weight initialization
+        :param lambda_: learning rate parameter (float)
+        :param eps: smoothing term (float)
+        :param s0: learning rate parameter (float)
+        :param p: learning rate parameter (float)
+        """
+        super().__init__()
+        self.eta = lambda k: lambda_ * (s0 / (s0 + k)) ** p
+        self.eps = eps
+        self.w = np.copy(w0)
+        self.g = 0
+
+    def update_weights(self, gradient: np.ndarray, iteration: int) -> np.ndarray:
+        """
+        Changing weights with respect to gradient
+        :param iteration: iteration number
+        :param gradient: gradient estimate
+        :return: weight difference: np.ndarray
+        """
+        # TODO: implement updating weights function
+        raise NotImplementedError('Adagrad update_weights function not implemented')
+
+    def calc_gradient(self, X: np.ndarray, y: np.ndarray) -> np.ndarray:
+        """
+        Getting objects, calculating gradient at point w
+        :param X: objects' features
+        :param y: objects' targets
+        :return: gradient: np.ndarray
+        """
+        # TODO: implement calculating gradient function
+        raise NotImplementedError('Adagrad calc_gradient function not implemented')
+
+
+class GradientDescentReg(GradientDescent):
+    """
+    Full gradient descent with regularization class
+    """
+
+    def __init__(self, w0: np.ndarray, lambda_: float, mu: float = mu_default, s0: float = s0_default,
+                 p: float = p_default):
+        """
+        :param mu: l2 coefficient
+        """
+        super().__init__(w0=w0, lambda_=lambda_, s0=s0, p=p)
+        self.mu = mu
+
+    def update_weights(self, gradient: np.ndarray, iteration: int) -> np.ndarray:
+        return super().update_weights(gradient, iteration)
+
+    def calc_gradient(self, X: np.ndarray, y: np.ndarray) -> np.ndarray:
+        l2 = None  # TODO
+        return super().calc_gradient(X, y) + l2 * self.mu
+
+
+class StochasticDescentReg(StochasticDescent):
+    """
+    Stochastic gradient descent with regularization class
+    """
+
+    def __init__(self, w0: np.ndarray, lambda_: float, mu: float = mu_default, s0: float = s0_default,
+                 p: float = p_default, batch_size: int = batch_size_default):
+        """
+        :param mu: l2 coefficient
+        """
+        super().__init__(w0=w0, lambda_=lambda_, s0=s0, p=p, batch_size=batch_size)
+        self.mu = mu
+
+    def update_weights(self, gradient: np.ndarray, iteration: int) -> np.ndarray:
+        return super().update_weights(gradient, iteration)
+
+    def calc_gradient(self, X: np.ndarray, y: np.ndarray) -> np.ndarray:
+        l2 = None  # TODO
+        return super().calc_gradient(X, y) + l2 * self.mu
+
+
+class MomentumDescentReg(MomentumDescent):
+    """
+    Momentum gradient descent with regularization class
+    """
+
+    def __init__(self, w0: np.ndarray, lambda_: float, alpha: float = alpha_default, mu: float = mu_default,
+                 s0: float = s0_default, p: float = p_default):
+        """
+        :param mu: l2 coefficient
+        """
+        super().__init__(w0=w0, lambda_=lambda_, alpha=alpha, s0=s0, p=p)
+        self.mu = mu
+
+    def update_weights(self, gradient: np.ndarray, iteration: int) -> np.ndarray:
+        return super().update_weights(gradient, iteration)
+
+    def calc_gradient(self, X: np.ndarray, y: np.ndarray) -> np.ndarray:
+        l2 = None  # TODO
+        return super().calc_gradient(X, y) + l2 * self.mu
+
+
+class AdagradReg(Adagrad):
+    """
+    Adaptive gradient algorithm with regularization class
+    """
+
+    def __init__(self, w0: np.ndarray, lambda_: float, eps: float = eps_default, mu: float = mu_default,
+                 s0: float = s0_default, p: float = p_default):
+        """
+        :param mu: l2 coefficient
+        """
+        super().__init__(w0=w0, lambda_=lambda_, eps=eps, s0=s0, p=p)
+        self.mu = mu
+
+    def update_weights(self, gradient: np.ndarray, iteration: int) -> np.ndarray:
+        return super().update_weights(gradient, iteration)
+
+    def calc_gradient(self, X: np.ndarray, y: np.ndarray) -> np.ndarray:
+        l2 = None  # TODO
+        return super().calc_gradient(X, y) + l2 * self.mu
+
+
+class LinearRegression:
+    """
+    Linear regression class
+    """
+
+    def __init__(self, descent, tolerance: float = tolerance_default, max_iter: int = max_iter_default):
+        """
+        :param descent: Descent class
+        :param tolerance: float stopping criterion for euclidean norm of weight difference
+        :param max_iter: int stopping criterion for iterations
+        """
+        self.descent = descent
+        self.tolerance = tolerance
+        self.max_iter = max_iter
+        self.loss_history = []
+
+    def fit(self, X: np.ndarray, y: np.ndarray) -> LinearRegression:
+        """
+        Getting objects, fitting descent weights
+        :param X: objects' features
+        :param y: objects' target
+        :return: self
+        """
+        # TODO: fit weights to X and y
+        raise NotImplementedError('LinearRegression fit function not implemented')
+
+    def predict(self, X: np.ndarray) -> np.ndarray:
+        """
+        Getting objects, predicting targets
+        :param X: objects' features
+        :return: predicted targets
+        """
+        # TODO: calculate prediction for X
+        raise NotImplementedError('LinearRegression predict function not implemented')
+
+    def calc_loss(self, X: np.ndarray, y: np.ndarray) -> None:
+        """
+        Getting objects, calculating loss
+        :param X: objects' features
+        :param y: objects' target
+        """
+        # TODO: calculate loss and save it to loss_history
+        raise NotImplementedError('LinearRegression calc_loss function not implemented')
+
+
+###########################################################
+####################### BONUS TASK ########################
+###########################################################
+
+
+class StochasticAverageGradient(BaseDescent):
+    """
+    Stochastic average gradient class (BONUS TASK)
+    """
+
+    def __init__(self, w0: np.ndarray, lambda_: float, x_shape: int, s0: float = s0_default, p: float = p_default):
+        """
+        :param w0: weight initialization
+        :param lambda_: learning rate parameter (float)
+        :param s0: learning rate parameter (float)
+        :param p: learning rate parameter (float)
+        """
+        super().__init__()
+        self.eta = lambda k: lambda_ * (s0 / (s0 + k)) ** p
+        self.w = np.copy(w0)
+        self.v = np.zeros((x_shape, w0.shape[0]))
+        self.d = 0
+
+    def update_weights(self, gradient: np.ndarray, iteration: int) -> np.ndarray:
+        """
+        Changing weights with respect to gradient
+        :param iteration: iteration number
+        :param gradient: gradient
+        :return: weight difference: np.ndarray
+        """
+        # TODO: implement updating weights function
+        raise NotImplementedError('GradientDescent update_weights function not implemented')
+
+    def calc_gradient(self, X: np.ndarray, y: np.ndarray) -> np.ndarray:
+        """
+        Getting objects, calculating gradient at point w
+        :param X: objects' features
+        :param y: objects' targets
+        :return: gradient: np.ndarray
+        """
+        # TODO: implement calculating gradient function
+        raise NotImplementedError('GradientDescent calc_gradient function not implemented')
+
+###########################################################
+####################### BONUS TASK ########################
+###########################################################
