d2b861a9af17174265350bcadc546349016a1443
fdrose <110309049+fdrose@users.noreply.github.com>
2024-02-28 11:58:38 +0300

    Add files via upload

---
+++ 2023-spring/homeworks-practice/homework-practice-09-em/preprocessing.py
@@ -0,0 +1,82 @@
+from dataclasses import dataclass
+from typing import Dict, List, Tuple
+
+import numpy as np
+
+
+@dataclass(frozen=True)
+class SentencePair:
+    """
+    Contains lists of tokens (strings) for source and target sentence
+    """
+    source: List[str]
+    target: List[str]
+
+
+@dataclass(frozen=True)
+class TokenizedSentencePair:
+    """
+    Contains arrays of token vocabulary indices (preferably np.int32) for source and target sentence
+    """
+    source_tokens: np.ndarray
+    target_tokens: np.ndarray
+
+
+@dataclass(frozen=True)
+class LabeledAlignment:
+    """
+    Contains arrays of alignments (lists of tuples (source_pos, target_pos)) for a given sentence.
+    Positions are numbered from 1.
+    """
+    sure: List[Tuple[int, int]]
+    possible: List[Tuple[int, int]]
+
+
+def extract_sentences(filename: str) -> Tuple[List[SentencePair], List[LabeledAlignment]]:
+    """
+    Given a file with tokenized parallel sentences and alignments in XML format, return a list of sentence pairs
+    and alignments for each sentence.
+
+    Args:
+        filename: Name of the file containing XML markup for labeled alignments
+
+    Returns:
+        sentence_pairs: list of `SentencePair`s for each sentence in the file
+        alignments: list of `LabeledAlignment`s corresponding to these sentences
+    """
+
+
+pass
+
+
+def get_token_to_index(sentence_pairs: List[SentencePair], freq_cutoff=None) -> Tuple[Dict[str, int], Dict[str, int]]:
+    """
+    Given a parallel corpus, create two dictionaries token->index for source and target language.
+
+    Args:
+        sentence_pairs: list of `SentencePair`s for token frequency estimation
+        freq_cutoff: if not None, keep only freq_cutoff most frequent tokens in each language
+
+    Returns:
+        source_dict: mapping of token to a unique number (from 0 to vocabulary size) for source language
+        target_dict: mapping of token to a unique number (from 0 to vocabulary size) target language
+
+    """
+    pass
+
+
+def tokenize_sents(sentence_pairs: List[SentencePair], source_dict, target_dict) -> List[TokenizedSentencePair]:
+    """
+    Given a parallel corpus and token_to_index for each language, transform each pair of sentences from lists
+    of strings to arrays of integers. If either source or target sentence has no tokens that occur in corresponding
+    token_to_index, do not include this pair in the result.
+    
+    Args:
+        sentence_pairs: list of `SentencePair`s for transformation
+        source_dict: mapping of token to a unique number for source language
+        target_dict: mapping of token to a unique number for target language
+
+    Returns:
+        tokenized_sentence_pairs: sentences from sentence_pairs, tokenized using source_dict and target_dict
+    """
+    pass
