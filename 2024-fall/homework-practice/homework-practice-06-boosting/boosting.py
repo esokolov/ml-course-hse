a31ce2443d7d795a929fb262e8aae8bd9720613e
Lerostre <72521090+Lerostre@users.noreply.github.com>
2024-12-12 20:24:22 +0300

    ahh boostings, finally...

---
+++ 2024-fall/homework-practice/homework-practice-06-boosting/boosting.py
@@ -0,0 +1,72 @@
+from __future__ import annotations
+
+from collections import defaultdict
+
+import numpy as np
+from sklearn.metrics import roc_auc_score
+from sklearn.tree import DecisionTreeRegressor
+
+from typing import Optional
+
+
+def score(clf, x, y):
+    return roc_auc_score(y == 1, clf.predict_proba(x)[:, 1])
+
+
+class Boosting:
+
+    def __init__(
+        self,
+        base_model_params: Optional[dict] = None,
+        n_estimators: int = 10,
+        learning_rate: float = 0.1,
+        plot: bool = False,
+    ):
+        self.base_model_class = DecisionTreeRegressor
+        self.base_model_params: dict = {} if base_model_params is None else base_model_params
+
+        self.n_estimators: int = n_estimators
+
+        self.models: list = []
+        self.gammas: list = []
+
+        self.learning_rate: float = learning_rate
+
+        self.plot: bool = plot
+
+        self.history = defaultdict(list)
+
+        self.sigmoid = lambda x: 1 / (1 + np.exp(-x))
+        self.loss_fn = lambda y, z: -np.log(self.sigmoid(y * z)).mean()
+        self.loss_derivative = lambda y, z: -y * self.sigmoid(-y * z)
+
+    def fit_new_base_model(self, x, y, predictions):
+        self.gammas.append()
+        self.models.append()
+
+    def fit(self, x_train, y_train, x_valid, y_valid):
+        """
+        :param x_train: features array (train set)
+        :param y_train: targets array (train set)
+        :param x_valid: features array (validation set)
+        :param y_valid: targets array (validation set)
+        """
+        train_predictions = np.zeros(y_train.shape[0])
+        valid_predictions = np.zeros(y_valid.shape[0])
+
+        for _ in range(self.n_estimators):
+            self.fit_new_base_model()
+
+        if self.plot:
+            pass
+
+    def predict_proba(self, x):
+        raise Exception("predict_proba not implemented")
+
+    def find_optimal_gamma(self, y, old_predictions, new_predictions) -> float:
+        gammas = np.linspace(start=0, stop=1, num=100)
+        losses = [self.loss_fn(y, old_predictions + gamma * new_predictions) for gamma in gammas]
+        return gammas[np.argmin(losses)]
+
+    def score(self, x, y):
+        return score(self, x, y)
